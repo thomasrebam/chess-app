95bb0724daaca730acb58fcae35f200ce8cce064
Thomas Renaud <thomasre@bam.tech>
2023-11-24 17:33:28 +0100

    modify pieces to be displayed at the right size

--- src/shared/views/Piece/Piece.tsx
+++ src/shared/views/Piece/Piece.tsx
@@ -1,20 +1,21 @@
 import {Image} from 'react-native';
 import {Player} from '../../domain/entities/Player';
 import {ColoredPieceName, PieceName} from '../../domain/entities/PieceName';
+import styled from '@emotion/native';
 
 export const PIECES: Record<ColoredPieceName, number> = {
-  wk: require('../../../../assets/WhiteKing.png'),
-  bk: require('../../../../assets/BlackKing.png'),
-  wq: require('../../../../assets/WhiteQueen.png'),
-  bq: require('../../../../assets/BlackQueen.png'),
-  wb: require('../../../../assets/WhiteBishop.png'),
-  bb: require('../../../../assets/BlackBishop.png'),
-  wn: require('../../../../assets/WhiteKnight.png'),
-  bn: require('../../../../assets/BlackKnight.png'),
-  wr: require('../../../../assets/WhiteRook.png'),
-  br: require('../../../../assets/BlackRook.png'),
-  wp: require('../../../../assets/WhitePawn.png'),
-  bp: require('../../../../assets/BlackPawn.png'),
+  wk: require('../../../../assets/images/WhiteKing.png'),
+  bk: require('../../../../assets/images/BlackKing.png'),
+  wq: require('../../../../assets/images/WhiteQueen.png'),
+  bq: require('../../../../assets/images/BlackQueen.png'),
+  wb: require('../../../../assets/images/WhiteBishop.png'),
+  bb: require('../../../../assets/images/BlackBishop.png'),
+  wn: require('../../../../assets/images/WhiteKnight.png'),
+  bn: require('../../../../assets/images/BlackKnight.png'),
+  wr: require('../../../../assets/images/WhiteRook.png'),
+  br: require('../../../../assets/images/BlackRook.png'),
+  wp: require('../../../../assets/images/WhitePawn.png'),
+  bp: require('../../../../assets/images/BlackPawn.png'),
 };
 
 interface PieceProps {
@@ -24,5 +25,11 @@ interface PieceProps {
 
 export const Piece = ({piece, player}: PieceProps) => {
   const coloredPieceName: ColoredPieceName = `${player}${piece}`;
-  return <Image source={PIECES[coloredPieceName]} />;
+  return <StyledImage source={PIECES[coloredPieceName]} />;
 };
+
+const StyledImage = styled(Image)({
+  flexShrink: 1,
+  width: '12.5%',
+  height: '12.5%',
+});
