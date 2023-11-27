b4b19710afedc9394ce072bbd9e9f92b179e0232
Thomas Renaud <thomasre@bam.tech>
2023-11-27 16:31:10 +0100

    modify helper to give position on the board

--- src/shared/views/helpers/getAbsolutePositionFromAlgebraicPosition.ts
+++ src/shared/views/helpers/getAbsolutePositionFromAlgebraicPosition.ts
@@ -1,12 +1,20 @@
 import {SIZE} from '../Piece/Piece';
 import {getFileCodeFromFile} from './getFileCodeFromFile';
 
+type AlgebraicPosition = {
+  file: string;
+  column: number;
+};
+
 export const getAbsolutePositionFromAlgebraicPosition = ({
   file,
   column,
-}: {
-  file: string;
-  column: number;
-}) => {
+}: AlgebraicPosition) => {
   return {x: getFileCodeFromFile(file) * SIZE, y: (8 - column) * SIZE};
+};
+
+export const getAbsolutePositionFromAlgebraicNotation = (notation: string) => {
+  const file = notation[0];
+  const column = Number(notation[1]);
+  return getAbsolutePositionFromAlgebraicPosition({file, column});
 };
