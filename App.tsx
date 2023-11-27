43b51397a41962ac27cc0a23b85e86c44f21a935
Thomas Renaud <thomasre@bam.tech>
2023-11-27 17:41:55 +0100

    Display played moves

--- App.tsx
+++ App.tsx
@@ -7,21 +7,28 @@
 
 import styled from '@emotion/native';
 import {ChessBoard} from './src/shared/views/ChessBoard/ChessBoard';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {GestureHandlerRootView, ScrollView} from 'react-native-gesture-handler';
 import 'react-native-gesture-handler';
+import {PlayedMoves} from './src/modules/playedMoves/PlayedMoves/PlayedMoves';
+import {Spacer} from './src/shared/views/Spacer/Spacer';
 
 function App(): JSX.Element {
   return (
-    <AppBackground>
-      <ChessBoard />
-    </AppBackground>
+    <StyledGestureHandlerRootView>
+      <AppBackground>
+        <Spacer height={32} />
+        <ChessBoard />
+        <PlayedMoves playedMoves={['e4', 'e5']} />
+      </AppBackground>
+    </StyledGestureHandlerRootView>
   );
 }
 
-const AppBackground = styled(GestureHandlerRootView)({
-  flex: 1,
-  justifyContent: 'center',
+const AppBackground = styled(ScrollView)({
+  flexGrow: 1,
   backgroundColor: 'black',
 });
+
+const StyledGestureHandlerRootView = styled(GestureHandlerRootView)({flex: 1});
 
 export default App;
