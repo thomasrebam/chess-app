6617ba284ad48b39b0f3ef9a6e9434d0f712d408
Thomas Renaud <thomasre@bam.tech>
2023-11-27 19:28:21 +0100

    Create a last move context to display on screen

--- src/shared/views/ChessBoard/ChessBoard.tsx
+++ src/shared/views/ChessBoard/ChessBoard.tsx
@@ -4,6 +4,8 @@ import {ChessBoardBackground} from './ChessBoardBackground';
 import {useRef, useState} from 'react';
 import {Piece} from '../Piece/Piece';
 import {Chess} from 'chess.js';
+import {LastMoveContext} from './LastMoveContext';
+import {getFileCodeFromFile} from '../helpers/getFileCodeFromFile';
 
 export const ChessBoard = () => {
   const chess = useRef(new Chess());
@@ -17,24 +19,42 @@ export const ChessBoard = () => {
       board: chess.current.board(),
     });
   };
+  const lastMove = chess.current.history({verbose: true})[
+    chess.current.history().length - 1
+  ];
+  const {row, column} = lastMove
+    ? {
+        row: getFileCodeFromFile(lastMove.to[0]),
+        column: Number(lastMove.to[1]),
+      }
+    : {
+        row: -1,
+        column: -1,
+      };
   return (
     <Container>
-      <ChessBoardBackground />
-      {gameState.board.map(file =>
-        file.map((square, index) => {
-          if (square === null) return null;
-          return (
-            <Piece
-              key={index}
-              piece={square.type}
-              player={square.color}
-              position={square.square}
-              chess={chess.current}
-              onTurn={onTurn}
-            />
-          );
-        }),
-      )}
+      <LastMoveContext.Provider
+        value={{
+          row,
+          column,
+        }}>
+        <ChessBoardBackground />
+        {gameState.board.map(file =>
+          file.map((square, index) => {
+            if (square === null) return null;
+            return (
+              <Piece
+                key={index}
+                piece={square.type}
+                player={square.color}
+                position={square.square}
+                chess={chess.current}
+                onTurn={onTurn}
+              />
+            );
+          }),
+        )}
+      </LastMoveContext.Provider>
     </Container>
   );
 };
