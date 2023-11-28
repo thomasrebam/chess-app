8fe4b2512faa6d2c03e66270b3e68296353161b7
Thomas Renaud <thomasre@bam.tech>
2023-11-28 02:09:38 +0100

    Switch chess board to analysis page (pas très propre mais on va faire avec pour l'instant)

--- src/app/navigation/AnalysisPage.tsx
+++ src/app/navigation/AnalysisPage.tsx
@@ -1,12 +1,45 @@
-import {PlayedMoves} from '../../modules/playedMoves/PlayedMoves/PlayedMoves';
+import {useRef, useState} from 'react';
 import {PlayedMovesProvider} from '../../modules/playedMoves/PlayedMovesContext/PlayedMoveContext';
 import {ChessBoard} from '../../shared/views/components/ChessBoard/ChessBoard';
+import {Chess} from 'chess.js';
+import {getFileCodeFromFile} from '../../shared/views/helpers/getFileCodeFromFile';
 import {Spacer} from '../../shared/views/components/Spacer/Spacer';
+import {PlayedMoves} from '../../modules/playedMoves/PlayedMoves/PlayedMoves';
+import {LastMoveContext} from '../../shared/views/components/ChessBoard/LastMoveContext';
 
 export const AnalysisPage = () => {
+  const chess = useRef(new Chess());
+  const [gameState, setGameState] = useState({
+    player: chess.current.turn(),
+    board: chess.current.board(),
+  });
+  const onTurn = () => {
+    setGameState({
+      player: gameState.player === 'w' ? 'b' : 'w',
+      board: chess.current.board(),
+    });
+  };
+  const lastMove = chess.current.history({verbose: true})[
+    chess.current.history().length - 1
+  ];
+  const {row, column} = lastMove
+    ? {
+        column: getFileCodeFromFile(lastMove.to[0]),
+        row: Number(lastMove.to[1]),
+      }
+    : {
+        row: -1,
+        column: -1,
+      };
   return (
     <PlayedMovesProvider>
-      <ChessBoard />
+      <LastMoveContext.Provider
+        value={{
+          row,
+          column,
+        }}>
+        <ChessBoard game={gameState} onTurn={onTurn} chess={chess} />
+      </LastMoveContext.Provider>
       <Spacer height={4} />
       <PlayedMoves />
     </PlayedMovesProvider>
--- src/shared/views/components/ChessBoard/ChessBoard.tsx
+++ src/shared/views/components/ChessBoard/ChessBoard.tsx
@@ -1,60 +1,42 @@
 import styled from '@emotion/native';
 import {View} from 'react-native';
 import {ChessBoardBackground} from './ChessBoardBackground';
-import {useRef, useState} from 'react';
 import {Piece} from '../Piece/Piece';
-import {Chess} from 'chess.js';
-import {LastMoveContext} from './LastMoveContext';
-import {getFileCodeFromFile} from '../../helpers/getFileCodeFromFile';
+import {Chess, Color, PieceSymbol, Square} from 'chess.js';
+import {MutableRefObject} from 'react';
 
-export const ChessBoard = () => {
-  const chess = useRef(new Chess());
-  const [gameState, setGameState] = useState({
-    player: chess.current.turn(),
-    board: chess.current.board(),
-  });
-  const onTurn = () => {
-    setGameState({
-      player: gameState.player === 'w' ? 'b' : 'w',
-      board: chess.current.board(),
-    });
+interface ChessBoardProps {
+  game: {
+    player: Color;
+    board: ({
+      square: Square;
+      type: PieceSymbol;
+      color: Color;
+    } | null)[][];
   };
-  const lastMove = chess.current.history({verbose: true})[
-    chess.current.history().length - 1
-  ];
-  const {row, column} = lastMove
-    ? {
-        column: getFileCodeFromFile(lastMove.to[0]),
-        row: Number(lastMove.to[1]),
-      }
-    : {
-        row: -1,
-        column: -1,
-      };
+  onTurn: () => void;
+  chess: MutableRefObject<Chess>;
+}
+
+export const ChessBoard = ({game, onTurn, chess}: ChessBoardProps) => {
   return (
     <Container>
-      <LastMoveContext.Provider
-        value={{
-          row,
-          column,
-        }}>
-        <ChessBoardBackground />
-        {gameState.board.map(file =>
-          file.map((square, index) => {
-            if (square === null) return null;
-            return (
-              <Piece
-                key={index}
-                piece={square.type}
-                player={square.color}
-                position={square.square}
-                chess={chess.current}
-                onTurn={onTurn}
-              />
-            );
-          }),
-        )}
-      </LastMoveContext.Provider>
+      <ChessBoardBackground />
+      {game.board.map(file =>
+        file.map((square, index) => {
+          if (square === null) return null;
+          return (
+            <Piece
+              key={index}
+              piece={square.type}
+              player={square.color}
+              position={square.square}
+              chess={chess.current}
+              onTurn={onTurn}
+            />
+          );
+        }),
+      )}
     </Container>
   );
 };
