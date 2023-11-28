a6598afeea14723230ee314fe55e75a5acd8f578
Thomas Renaud <thomasre@bam.tech>
2023-11-28 01:41:46 +0100

    Clean last move context

--- src/shared/views/components/ChessBoard/LastMoveContext.tsx
+++ src/shared/views/components/ChessBoard/LastMoveContext.tsx
@@ -4,19 +4,3 @@ export const LastMoveContext = createContext<{row: number; column: number}>({
   row: -1,
   column: -1,
 });
-
-// interface LastMoveProviderProps {
-//   children: ReactNode;
-// }
-
-// export const LastMoveProvider = ({children}: LastMoveProviderProps) => {
-//   const [lastMove, setLastMove] = useState<{row: number; column: number}>({
-//     row: -1,
-//     column: -1,
-//   });
-//   return (
-//     <LastMoveContext.Provider value={lastMove}>
-//       {children}
-//     </LastMoveContext.Provider>
-//   );
-// };
