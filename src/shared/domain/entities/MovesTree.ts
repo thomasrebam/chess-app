d60b846e9456e4f35983be04249593e9202da27e
Thomas Renaud <thomasre@bam.tech>
2023-12-08 14:20:22 +0100

    Add moves tree type

---
+++ src/shared/domain/entities/MovesTree.ts
@@ -0,0 +1,23 @@
+import {Player} from './Player';
+
+type MoveInformation = {
+  move: string;
+  fen: string;
+  children: string[];
+  parentKey: string;
+  player: Player;
+  moveDepth: number;
+};
+
+export type MovesTree = Record<string, MoveInformation>;
+
+export const emptyMovesTree: MovesTree = {
+  empty: {
+    move: '',
+    fen: '',
+    player: 'w',
+    children: [],
+    parentKey: '-1',
+    moveDepth: 0,
+  },
+};
