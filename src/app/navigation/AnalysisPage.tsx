05c54995d5a1058e9feb320e99d7f1f381a3629f
Thomas Renaud <thomasre@bam.tech>
2023-12-12 17:22:04 +0100

    Add pgn load from menu page

--- src/app/navigation/AnalysisPage.tsx
+++ src/app/navigation/AnalysisPage.tsx
@@ -8,9 +8,20 @@ import {View} from 'react-native';
 import styled from '@emotion/native';
 import {AnalysisBottomBar} from '../../modules/bottomBar/AnalysisBottomBar/AnalysisBottomBar';
 import {ChessEngineProvider} from '../../shared/views/contexts/ChessEngineContext';
+import {RouteProp, useRoute} from '@react-navigation/native';
+import {AuthenticatedNavigatorStackParamList} from './AuthenticatedNavigator/AuthenticatedNavigator.type';
 
 export const AnalysisPage = () => {
+  const {params} =
+    useRoute<RouteProp<AuthenticatedNavigatorStackParamList, 'Analysis'>>();
   const chess = useRef(new Chess());
+  if (params.pgn) {
+    const pgn = params.pgn;
+    if (pgn.includes(']')) {
+      const pgnSplit = pgn.split(']');
+      chess.current.loadPgn(pgnSplit[pgnSplit.length - 1]);
+    } else chess.current.loadPgn(pgn);
+  }
 
   const [gameState, setGameState] = useState({
     player: chess.current.turn(),
--- src/app/navigation/AuthenticatedNavigator/AuthenticatedNavigator.type.ts
+++ src/app/navigation/AuthenticatedNavigator/AuthenticatedNavigator.type.ts
@@ -1,4 +1,4 @@
 export type AuthenticatedNavigatorStackParamList = {
   Menu: undefined;
-  Analysis: undefined;
+  Analysis: {pgn: string | undefined};
 };
