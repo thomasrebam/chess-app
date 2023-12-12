48b8ff0951cbbcc210523a0ee7c0eb433de10e93
Thomas Renaud <thomasre@bam.tech>
2023-12-12 22:05:57 +0100

    Add a testing system

--- src/app/navigation/TestingPage.tsx
+++ src/app/navigation/TestingPage.tsx
@@ -2,11 +2,12 @@ import {useRef} from 'react';
 import {PlayedMovesProvider} from '../../modules/playedMoves/PlayedMovesContext/PlayedMoveContext';
 import {ChessEngineProvider} from '../../shared/views/contexts/ChessEngineContext';
 import {Chess} from 'chess.js';
-import {ChessBoard} from '../../shared/views/components/ChessBoard/ChessBoard';
 import styled from '@emotion/native';
 import {View} from 'react-native';
 import {Spacer} from '../../shared/views/components/Spacer/Spacer';
 import {Typography} from '../../shared/boson/components/Typography/Typography';
+import {PlayedMoves} from '../../modules/playedMoves/PlayedMoves/PlayedMoves';
+import {TestingBoard} from '../../modules/testingBoard/TestingBoard';
 
 export const TestingPage = () => {
   const chess = useRef(new Chess());
@@ -15,11 +16,12 @@ export const TestingPage = () => {
     <PlayedMovesProvider value={{playedMoves: undefined}}>
       <ChessEngineProvider value={{chess}}>
         <Container>
-          <ChessBoard />
+          <TestingBoard pgn={['e4', 'e5', 'Nf3', 'Nf6', 'Nxe5', 'Nxe4']} />
           <Spacer height={32} />
           <BottomContentContainer>
             <StyledText>What to do in this position ?</StyledText>
           </BottomContentContainer>
+          <PlayedMoves />
         </Container>
       </ChessEngineProvider>
     </PlayedMovesProvider>
---
+++ src/modules/testingBoard/TestingBoard.tsx
@@ -0,0 +1,35 @@
+import {useContext, useEffect, useState} from 'react';
+import {ChessBoard} from '../../shared/views/components/ChessBoard/ChessBoard';
+import {PlayedMovesContext} from '../playedMoves/PlayedMovesContext/PlayedMoveContext';
+import {ChessEngineContext} from '../../shared/views/contexts/ChessEngineContext';
+
+interface TestingBoardProps {
+  pgn: string[];
+}
+
+export const TestingBoard = ({pgn}: TestingBoardProps) => {
+  const [currentMove, setCurrentMove] = useState(0);
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  const {addPlayedMove, currentMoveKey} = useContext(PlayedMovesContext);
+  const {chess} = useContext(ChessEngineContext);
+
+  useEffect(() => {
+    const history = chess.current.history({verbose: true});
+    if (
+      history[history.length - 1] &&
+      history[history.length - 1].color === 'w' &&
+      history[history.length - 1].san === pgn[currentMove]
+    ) {
+      const move = pgn[currentMove + 1];
+      chess.current.move(move);
+      const newHistory = chess.current.history({verbose: true});
+      addPlayedMove({
+        move,
+        fen: chess.current.fen(),
+        squareTo: newHistory[newHistory.length - 1].to,
+      });
+      setCurrentMove(currentMove + 2);
+    }
+  }, [chess, pgn, currentMove, addPlayedMove]);
+  return <ChessBoard />;
+};
