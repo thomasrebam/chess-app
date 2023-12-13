508f9c45364d2218e0b6b3b2ba0ecc65dd3caf6c
Thomas Renaud <thomasre@bam.tech>
2023-12-14 00:24:40 +0100

    Clean modal display

--- src/modules/analysis/AnalysisModal/AnalysisModal.tsx
+++ src/modules/analysis/AnalysisModal/AnalysisModal.tsx
@@ -2,11 +2,12 @@ import styled from '@emotion/native';
 import {Text, View} from 'react-native';
 import Modal from 'react-native-modal';
 import {Button} from '../../../shared/boson/components/Button/Button';
-import {Spacer} from '../../../shared/views/components/Spacer/Spacer';
 import {PersistentStorageService} from '../../../shared/views/services/PersistentStorageService';
 import {useContext} from 'react';
 import {PlayedMovesContext} from '../../playedMoves/PlayedMovesContext/PlayedMoveContext';
 import {SavedAnalysisContext} from '../../../shared/views/contexts/SavedAnalysisContext';
+import {TextInput} from '../../../shared/boson/components/TextInput/TextInput';
+import {Spacer} from '../../../shared/views/components/Spacer/Spacer';
 
 interface AnalysisModalProps {
   isModalVisible: boolean;
@@ -40,12 +41,16 @@ export const AnalysisModal = ({
         <StyledText>
           You are saving this analysis, please give it a name
         </StyledText>
-        <Spacer height={4} />
-        <ButtonContainer>
-          <Button.Primary label={'Save analysis'} onPress={onSavePress} />
-          <Spacer width={8} />
-          <Button.Secondary label={'Close'} onPress={onPressClose} />
-        </ButtonContainer>
+        <StyledTextInput placeholder="Analysis name" focusable />
+        <ButtonsContainer>
+          <ButtonContainer>
+            <Button.Primary label={'Save analysis'} onPress={onSavePress} />
+          </ButtonContainer>
+          <Spacer width={16} />
+          <ButtonContainer>
+            <Button.Secondary label={'Cancel'} onPress={onPressClose} />
+          </ButtonContainer>
+        </ButtonsContainer>
       </ModalContainer>
     </Modal>
   );
@@ -53,19 +58,27 @@ export const AnalysisModal = ({
 
 const ModalContainer = styled(View)({
   flexShrink: 1,
+  paddingHorizontal: 16,
   minHeight: 150,
   borderRadius: 16,
-  alignItems: 'center',
   justifyContent: 'space-evenly',
   backgroundColor: 'black',
 });
 
-const ButtonContainer = styled(View)({
+const ButtonsContainer = styled(View)({
   flexDirection: 'row',
-  justifyContent: 'space-between',
+  justifyContent: 'space-evenly',
   alignItems: 'center',
+});
+
+const ButtonContainer = styled(View)({
+  flex: 1,
 });
 
 const StyledText = styled(Text)({
   color: 'white',
+});
+
+const StyledTextInput = styled(TextInput)({
+  flex: 1,
 });
