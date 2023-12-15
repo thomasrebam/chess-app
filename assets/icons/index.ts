aac8a44f076e94d9875b9896c4949da94dbeab5e
Thomas Renaud <thomasre@bam.tech>
2023-12-15 16:57:51 +0100

    Add trash can icon

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -3,10 +3,12 @@ import {SvgProps} from 'react-native-svg';
 
 import LeftArrow from './LeftArrow.svg';
 import RightArrow from './RightArrow.svg';
+import TrashCan from './TrashCan.svg';
 
 export type SvgComponent = FunctionComponent<SvgProps>;
 
 export const Icon = {
   LeftArrow,
   RightArrow,
+  TrashCan,
 };
