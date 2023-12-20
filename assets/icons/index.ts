87f6068c4b6333837aa87d507cbca88909a99c55
Thomas Renaud <thomasre@bam.tech>
2023-12-20 15:08:20 +0100

    Add pieces as svg

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -6,6 +6,18 @@ import RightArrow from './RightArrow.svg';
 import TrashCan from './TrashCan.svg';
 import TrueCheck from './TrueCheck.svg';
 import FalseCross from './FalseCross.svg';
+import BlackKing from './BlackKing.svg';
+import BlackQueen from './BlackQueen.svg';
+import BlackBishop from './BlackBishop.svg';
+import BlackKnight from './BlackKnight.svg';
+import BlackRook from './BlackRook.svg';
+import BlackPawn from './BlackPawn.svg';
+import WhiteKing from './WhiteKing.svg';
+import WhiteQueen from './WhiteQueen.svg';
+import WhiteBishop from './WhiteBishop.svg';
+import WhiteKnight from './WhiteKnight.svg';
+import WhiteRook from './WhiteRook.svg';
+import WhitePawn from './WhitePawn.svg';
 
 export type SvgComponent = FunctionComponent<SvgProps>;
 
@@ -15,4 +27,16 @@ export const Icon = {
   TrashCan,
   TrueCheck,
   FalseCross,
+  BlackKing,
+  BlackQueen,
+  BlackBishop,
+  BlackKnight,
+  BlackRook,
+  BlackPawn,
+  WhiteKing,
+  WhiteQueen,
+  WhiteBishop,
+  WhiteKnight,
+  WhiteRook,
+  WhitePawn,
 };
